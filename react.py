ab5fb1907de6486b22c997c31ae4031b741f0dfa
phalgunakumarrachoor <pkrachoork@gmail.com>
2026-09-04 03:01:36 -0500

    functino calling reasoning

---
+++ react.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+load_dotenv()
+
+
+@tool
+def triple(num:float)-> float:
+    """ Param is a number, return the triple of that number """
+    return float(num) * 3
+
+
+tools = [TavilySearch(max_results=1), triple]
+
+llm = ChatOpenAI(model_name="gpt-4.0-mini", temperature=0).bind_tools(tools)
+
